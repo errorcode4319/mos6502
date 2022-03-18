8aa1a672a24dbc567c53978a854dfcc0c33ab6eb
sandlekang <sandlekang@gmail.com>
2022-03-19 00:03:30 +0900

    OSX BuildTest

---
+++ src/cpu/cpu.cpp
@@ -0,0 +1,13 @@
+#include "cpu.hpp"
+
+namespace cpu {
+
+CPU::CPU() {
+
+}
+
+CPU::~CPU() {
+
+}
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+#include "cpu/cpu.hpp"
+
+int main() {
+    std::cout << "MOS 6502 Emulator" << std::endl;
+
+    cpu::CPU processor;
+}
